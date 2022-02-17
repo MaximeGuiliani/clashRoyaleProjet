6b123192128526ba07f45caed6930f22fd859e26
MaximeGuiliani <maxime.guiliani@hotmail.fr>
2022-02-17 12:16:08 +0100

    WIP

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const express = require('express')
-
 const app = express()
 const port = 3000
 
 const {
     spawn
-} = require('child_process');
-const python = spawn('python3', ['python.py %232GPUV2Y0 %232L0YRYUQR']);
-python.stdout.on('data', function (data) {
-    dataToSend = data.toString();
+} = require("child_process");
 
+const py = spawn("python3", ['script.py', '%232GPUV2Y0', '%232L0YRYUQR', '%23C9PR9PJ02', '%23CQLGCCJ', '%232VUQ9YJ']);
+py.stdout.on("data", data => {
+    console.log(`stdout: ${data}`);
 });
 
-
 app.get('/', (req, res) => {
+
 
     res.sendFile(__dirname + '/index.html');
 })
--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import sys
 import requests
 import json
+print("start code <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
 
 ########################################
 data_player = 0
@@ -32,10 +33,26 @@ def create_json(id_player):
     response = requests.get(get_data_url(id_player))
     if response.status_code == 200:
         data_player = response.content.decode("utf-8")
-        jsonfile = id_player+'.json'
+        jsonfile = 'data/'+id_player+'.json'
     with open(jsonfile, 'w') as file:
         file.write(data_player)
         print(data_player)
+
+###########
+
+
+def get_name(id_player):
+    response = requests.get(begining_url + id_player +
+                            "?Authorization=Bearer " + my_key)
+    if response.status_code == 200:
+        data_player = response.content.decode("utf-8")
+        player_name = "data/"+"player_name"+id_player+'.json'
+    with open(player_name, 'w') as file:
+        file.write(data_player)
+        print(data_player)
+    with open(player_name) as json_file:
+        data = json.load(json_file)
+    return data['name']
 
 
 ########################################
@@ -43,7 +60,7 @@ def create_json(id_player):
 def get_chest(id_player):
     next_chest_name.clear()
     next_chest_index.clear()
-    jsonfile = id_player+'.json'
+    jsonfile = "data/"+id_player+'.json'
     with open(jsonfile) as json_file:
         data = json.load(json_file)
         for i in data['items']:
@@ -84,9 +101,9 @@ def image_balise(chest_name):
 ########################################
 
 
-def add_data_tohtml():
-    html_data = ""
-    print('lenght of next chest name ' + str(len(next_chest_name)))
+def add_data_tohtml(player):
+    html_data = "<h1 style='text-align:center'> %s </h1> " % get_name(player)
+
     for i in range(len(next_chest_name)):
         html_data = html_data + \
             str(image_balise(next_chest_name[i])) + \
@@ -99,12 +116,16 @@ def generate_html():
     for i in range(len(sys.argv)-1):
         create_json(str(sys.argv[i+1]))
         get_chest(str(sys.argv[i+1]))
-        add_data_tohtml()
-    final_html = html_data_start + \
-        html_body[0] + "<br>" + html_body[1] + html_data_end
+        add_data_tohtml(str(sys.argv[i+1]))
+    final_html = html_data_start
+    for j in range(len(html_body)):
+        final_html += html_body[j] + "<br>"
+    final_html += html_data_end
     with open('index.html', 'w') as html_file:
         html_file.write(final_html)
 
 
 ########################################
+print("generate_html")
 generate_html()
+print("end code <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
